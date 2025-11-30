e37fdde77ad33e2d289dda28c9bc37fe45082859
bharat <bharatchoutharyb@gmail.com>
2025-11-30 17:04:57 +0530

    day2

---
+++ script.js
@@ -0,0 +1,41 @@
+// Shery effects
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", { videos: ["./video/cover1.mp4", "./video/cover2.mp4", "./video/cover3.mp4"] });
+
+gsap.to(".fleftelm", {
+    scrollTrigger: {
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1
+    },
+    y: "-300%",
+    ease: Power1
+})
+
+let sections = document.querySelectorAll(".fleftelm")
+Shery.imageEffect(".images", {
+    style: 5,
+// config: {
+//   onMouse: { value: 1 }
+// },
+
+    slideStyle: (setScroll) => {
+        sections.forEach(function (section, index) {
+            ScrollTrigger.create({
+                trigger: section,
+                start: "top top",
+                scrub: 1,
+                onUpdate: function (prog) {
+                    setScroll(prog.progress + index)
+                }
+            })
+        })
+    }
+});
+
+
+
